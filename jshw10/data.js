9fccafecdc459788fb1c9565293e73f1167c1391
Алексей Якунин <116656993+Womxid@users.noreply.github.com>
2023-01-31 11:43:41 +0300

    Add files via upload

---
+++ jshw10/data.js
@@ -0,0 +1,29 @@
+const dataInfo = `[
+
+        
+        {
+        "id": 2,
+        "name": "space1",
+        "description": "reprehenderit est deserunt velit ipsam",
+        "url": "https://i.artfile.ru/1980x1320_1137825_[www.ArtFile.ru].jpg",
+        "width": 300,
+        "height": 300
+        
+        },
+        {
+            "id": 22,
+            "name": "space2",
+            "description": "reprehenderit est deserunt velit ipsam",
+            "url": "https://phonoteka.org/uploads/posts/2021-05/1622236025_7-phonoteka_org-p-kosmos-art-na-telefon-krasivo-7.jpg",
+            "width": 300,
+            "height": 300
+        },
+        {
+            "id": 222,
+            "name": "space3",
+            "description": "reprehenderit est deserunt velit ipsam",
+            "url": "https://kartinkin.net/uploads/posts/2021-07/1626791380_5-kartinkin-com-p-kosmos-krasivii-art-art-krasivo-5.jpg",
+            "width": 300,
+            "height": 300
+        }
+    ]`;
